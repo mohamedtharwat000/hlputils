60f9dabd9807e16c29c761f27d104c49ec3dccfd
mohamedtharwat000 <mohamed.sarwat.000@gmail.com>
2024-03-20 03:38:12 +0200

    add classType function

--- index.js
+++ index.js
@@ -1,17 +1,6 @@
-const type = require('./src/hlputils/type');
 const log = require('./src/hlputils/log');
+const type = require('./src/hlputils/type');
+const classType = require('./src/hlputils/classType');
 const objectProto = require('./src/hlputils/objectProto');
 
-let scope = globalThis;
-
-if (type(scope) === 'global') {
-  scope = global;
-}
-
-if (type(scope) === 'window') {
-  scope = window;
-}
-
-objectProto();
-scope.type = type;
-scope.log = log;
+module.exports = { log, type, classType, objectProto };

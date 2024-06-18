ef10d6b0a05e0ae0dc5b66635f494a68842537c5
Mohamed Tharwat <mohamed.sarwat.000@gmail.com>
2024-06-18 04:40:59 +0300

    Create prettier.config.js

---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+export default {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: false,
+  trailingComma: "es5",
+  bracketSpacing: true,
+  singleAttributePerLine: true,
+  bracketSameLine: false,
+  quoteProps: "consistent",
+  endOfLine: "auto",
+};

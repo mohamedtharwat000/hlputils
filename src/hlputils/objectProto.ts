28e433138b66cfd452a5547a450fffd97eceba55
Mohamed Tharwat <mohamed.sarwat.000@gmail.com>
2024-06-20 08:08:23 +0300

    Update objectProto.ts

--- src/hlputils/objectProto.ts
+++ src/hlputils/objectProto.ts
@@ -1,25 +1,22 @@
-import type from './type';
+import type from './type.js';
 
 /**
  * Enhances the `Object.prototype` by adding methods from the global `Object`.
  * These methods can be called on any object.
  */
 function objectProto(): void {
-  Object.getOwnPropertyNames(Object)
-    .filter((property: string) => type(Object[property]) === 'function')
-    .forEach((property: string) => {
-      // eslint-disable-next-line no-extend-native
-      Object.defineProperty(Object.prototype, property, {
-        configurable: false,
-        enumerable: false,
-        writable: true,
-        value(this: unknown, ...args: unknown[]) {
-          return type(this) === 'object'
-            ? Object[property](this, ...args)
-            : undefined;
+  const OPM = Object.getOwnPropertyDescriptors(Object);
+
+  for (const fn in OPM) {
+    if (type(OPM[fn]) === 'function') {
+      Object.defineProperty(Object.prototype, fn, {
+        ...OPM[fn],
+        value(...args: unknown[]) {
+          return type(this) === 'object' && OPM[fn].value(this, ...args);
         },
       });
-    });
+    }
+  }
 }
 
 export default objectProto;

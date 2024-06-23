b20a66c0af1733397c84880d510a51916fded43a
Mohamed Tharwat <mohamed.sarwat.000@gmail.com>
2024-06-23 05:49:21 +0300

    (feat) add class types

--- src/hlputils/type.ts
+++ src/hlputils/type.ts
@@ -5,7 +5,12 @@
  * @returns The type of the value (e.g., 'number', 'object', etc.).
  */
 function type(value: unknown): string {
-  return Object.prototype.toString.call(value).slice(8, -1).toLowerCase();
+  let type = Object.prototype.toString.call(value).slice(8, -1).toLowerCase();
+  if (type === 'function') {
+    type = Function.prototype.toString.call(value).slice(0, 8);
+    type = type.startsWith('class') ? 'class' : 'function';
+  }
+  return type;
 }
 
 export default type;
